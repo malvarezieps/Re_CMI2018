de236db42d9db135092d2f5d377e1fe847761928
m.alvarez <marco.alvarez@ieps.gob.ec>
2018-07-24 16:55:48 -0500

    Agregado funciones llenado de credito

--- arcEspeciales/fiilTablesData.php
+++ arcEspeciales/fiilTablesData.php
@@ -1,8 +1,7 @@
 <?php 
 include ('../lib/dbconfig.php');
 
-
-FillAsistencias();
+FillAccesoCredito();
 
 function FillVisitas()
 {
@@ -61,6 +60,86 @@ function FillAsistencias()
 		{
 			$sqlUpdate = "update fp_asesoria_asistencia_cofinanciamiento set tipo_asistencia = 'operativa' where cod_asesoria_asistencia_cofinanciamiento = " . $codLinea;
 		}
+
+		query($sqlUpdate);
+		echo $sqlUpdate . "<br>";
+	}
+
+	echo "Deal all works!!<br>";
+}
+
+function FillAccesoCredito()
+{
+	$sql = "select * from fp_asesoria_asistencia_cofinanciamiento where year(fecha_reporte) = 2018";
+	$res = query($sql);
+	$arrayDestino = array('Capital de Trabajo', 'Activos Productivos', 'Asistencia Tecnica', 'Otros');
+	$montoCredito = array('$1.000 A $5.000',
+							'$5.001 A $10.000',
+							'$10.001 A $15.000',
+							'$15.001 A $20.000',
+							'$20.001 A $30.000',
+							'$30.001 A $40.000',
+							'$40.001 A $50.000',
+							'$50.001 A $60.000',
+							'MAS DE $60.000');
+	$plazoCredito = array('DE 30 A 365 DIAS',
+							'DE 366 A 730 DIAS',
+							'DE 731 A 1.095 DIAS',
+							'MÁS DE TRES AÑOS');
+
+	$interesCredito = array('5% al 10%', '11% al 15%', 'mas 15%');
+	$acompanamientoCredito = array('1 a 3 visitas', '4 a 7 visitas', 'mas de 8 visitas');
+	$programaCredito = array('BanEcuador', 'Impulso Joven', 'Banco del Pueblo', 'Otros');
+
+	while($fila = mysql_fetch_array($res))
+	{
+		$codLinea = $fila['cod_asesoria_asistencia_cofinanciamiento'];
+		$opcRand = mt_rand(1, 2);
+		$sqlUpdate = '';
+
+		$valorMontoCredito = '';
+		$valorMontoPlazo = '';
+		$valorInteresCredito = '';
+		$valorAcompanamiento = '';
+		$valorProgramaProducto = '';
+		$esArticulado = '';
+		$accedeCredito = '';
+		$destinoCredito = '';
+		if($opcRand == 1)
+		{
+
+			// Tiene credito			
+
+			$randomico = mt_rand(0, 3);
+			$destinoCredito = $arrayDestino[$randomico];
+			$esArticulado = 'si';
+			$accedeCredito = 'si';			
+			$randomico = mt_rand(0, 9);
+			$valorMontoCredito = $montoCredito[$randomico];
+			$randomico = mt_rand(0, 3);
+			$valorMontoPlazo = $plazoCredito[$randomico];
+			$randomico = mt_rand(0, 2);
+			$valorInteresCredito = $interesCredito[$randomico];
+			$randomico = mt_rand(0, 2);
+			$valorAcompanamiento = $acompanamientoCredito[$randomico];
+			$randomico = mt_rand(0, 3);
+			$valorProgramaProducto = $programaCredito[$randomico];		
+			
+		}
+		else
+		{
+
+			$destinoCredito = 'no';
+			$esArticulado = 'no';
+			$accedeCredito = 'no';						
+			$valorMontoCredito = 0;			
+			$valorMontoPlazo = 0;
+			$valorInteresCredito = 0;			
+			$valorAcompanamiento = 'no';			
+			$valorProgramaProducto = 'no';
+		}
+
+		$sqlUpdate = "update fp_asesoria_asistencia_cofinanciamiento set destino_credito = '" . $destinoCredito . "', credito_articulado = '" . $esArticulado . "', accede_credito = '" . $accedeCredito . "', monto_credito = '" . $valorMontoCredito . "', plazo_credito = '" . $valorMontoPlazo . "', interes_credito = '" . $valorInteresCredito . "', acompanamiento = '" . $valorAcompanamiento . "', programa_producto = '" . $valorProgramaProducto . "'  where cod_asesoria_asistencia_cofinanciamiento = " . $codLinea;
 
 		query($sqlUpdate);
 		echo $sqlUpdate . "<br>";
